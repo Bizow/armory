d2ff309a0007cafd36c7da3b33adcbac5f53536b
Derrick Gremillion <derrick.gremillion@gmail.com>
2020-06-06 10:03:57 -0600

    add xbot.fbx

--- .blend/Sources/Main.hx
+++ .blend/Sources/Main.hx
@@ -4,10 +4,10 @@ class Main {
     public static inline var projectName = 'world';
     public static inline var projectPackage = 'arm';
     public static function main() {
-        iron.object.BoneAnimation.skinMaxBones = 8;
+        iron.object.BoneAnimation.skinMaxBones = 67;
         iron.object.LightObject.cascadeCount = 4;
         iron.object.LightObject.cascadeSplitFactor = 0.800000011920929;
-        armory.system.Starter.numAssets = 17;
+        armory.system.Starter.numAssets = 20;
         armory.system.Starter.drawLoading = armory.trait.internal.LoadingScreen.render;
         armory.system.Starter.main(
             'Scene',
